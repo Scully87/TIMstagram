2b442f09648be0134c75ee9d29feb9b9b3b267ba
Scully87 <scully87.ts@googlemail.com>
2014-11-07 14:56:00 +0000

    test: users fill out form to display a new post

--- Gemfile
+++ Gemfile
@@ -27,7 +27,12 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 
 gem 'rspec-rails', group: :test
+
 gem 'capybara', group: :test
+
+gem 'paperclip'
+
+gem 'devise'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,11 +1,38 @@
 require 'rails_helper'
 
 describe 'posts' do
-  context 'no posts have been added' do
+
+  context 'when no posts have been added' do
+
       it 'should display a prompt to add a post' do
       visit '/posts'
       expect(page).to have_content 'No posts yet!'
       expect(page).to have_link 'Add a post!'
     end
   end
+
+  context 'posts have been added' do
+
+    before do
+      Post.create(name: 'Little Cat')
+    end
+
+    it 'should display posts' do
+      visit '/posts'
+      expect(page).to have_content('Little Cat')
+      expect(page).not_to have_content('No posts yet!')
+    end
+  end
+
+  describe 'creating posts' do
+    it 'prompts user to fill out a form, then displays the new post' do
+    visit '/posts'
+    click_link 'Add a post'
+    fill_in 'Name', with: 'Little Cat'
+    click_button 'Create Post'
+    expect(page).to have_content 'Little Cat'
+    expect(current_path).to eq '/posts'
+   end
+  end
+
 end
