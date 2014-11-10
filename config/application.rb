fa19656c2034d445cb96958330742f94f3e93f7d
Scully87 <scully87.ts@googlemail.com>
2014-11-10 19:09:31 +0000

    config assets

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,6 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
-config.serve_static_assets = true
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -16,6 +15,7 @@ Bundler.require(*Rails.groups)
 
 module Instagram
   class Application < Rails::Application
+config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
