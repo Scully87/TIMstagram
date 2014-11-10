b4ae46208c7640567617a0d6a26f75849bced661
Scully87 <scully87.ts@googlemail.com>
2014-11-10 19:59:55 +0000

    heroku sizing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
 
-  has_attached_file :image, :styles => { :medium => "300#x300#", :thumb => "100#x100#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "500x500#", :thumb => "250x250#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   has_many :comments, dependent: :destroy
