875c2e8ac601bb90716db0824f02490395e8ca66
Scully87 <scully87.ts@googlemail.com>
2014-11-10 18:51:21 +0000

    env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   # Settings specified here will take precedence over those in config/application.rb.
 
